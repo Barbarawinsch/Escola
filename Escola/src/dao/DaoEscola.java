5cf21e0f1dc882c7ce0d76ca4640e0109faa8aed
Administrador <Administrador@10.10.4.26>
2019-09-12 16:31:32 -0300

    parou no inserir dao

---
+++ Escola/src/dao/DaoEscola.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+
+/**
+ *
+ * @author Administrador
+ */
+public class DaoEscola {
+    public static boolean inserir(Escola objeto) {
+        String sql = "INSERT INTO escola (nome, endereco, sigla, nralunos, area) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            ps.setString(3, objeto.getSigla());
+            ps.setInt(4, objeto.getNralunos());
+            ps.setInt(5, objeto.getArea());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+}
